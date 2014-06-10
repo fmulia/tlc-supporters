f95f3517afab66202d028fc356ebede0a3294bac
Sebastian <tsebastian@paypal.com>
2014-06-10 14:37:55 +0800

    TLC: added track impact page

--- views/track_impact.php
+++ views/track_impact.php
@@ -1,1 +1,12 @@
-<h1>Hello World!</h1>
+<div id="trackImpact">
+	<div id="donation">
+		<h1>Hello World!</h1>
+		<p>Enter details here</p>
+	</div>
+	<div id="showImpact">
+		
+	</div>
+	<div id="cardOptions">
+		
+	</div>
+</div>
