b19daa1372eedfb50891fa7607ec4666c6075116
Shipeng Xu <shipxu@paypal.com>
2014-06-12 17:50:31 +0800

    console.log

--- views/js/impacts.js
+++ views/js/impacts.js
@@ -1,39 +1,41 @@
 // Generate impact of a donation.
-// Shipeng Xu
-// shipxu@paypal.com
 
 function getImpacts(money)
 {
-	var impactData = [{desc:'a', value:1.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'b', value:2.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'c', value:3.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'d', value:4.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'e', value:5.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'f', value:6.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'g', value:7.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'h', value:8.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'i', value:9.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'j', value:10.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'k', value:11.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'l', value:12.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'m', value:13.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'n', value:14.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'o', value:15.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'p', value:16.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'q', value:17.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'r', value:18.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'s', value:19.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'t', value:20.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'u', value:21.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'v', value:22.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'},
-					  {desc:'w', value:23.0, url:'https://www.google.com.sg/images/srpr/logo11w.png'}];
+	var impactData = [{desc:'a', value:1.0, url:'https://.png', impact_instances:0},
+					  {desc:'b', value:2.0, url:'https://.png', impact_instances:0},
+					  {desc:'c', value:3.0, url:'https://.png', impact_instances:0},
+					  {desc:'d', value:4.0, url:'https://.png', impact_instances:0},
+					  {desc:'e', value:5.0, url:'https://.png', impact_instances:0},
+					  {desc:'f', value:6.0, url:'https://.png', impact_instances:0},
+					  {desc:'g', value:7.0, url:'https://.png', impact_instances:0},
+					  {desc:'h', value:8.0, url:'https://.png', impact_instances:0},
+					  {desc:'i', value:9.0, url:'https://.png', impact_instances:0},
+					  {desc:'j', value:10.0, url:'https://.png', impact_instances:0},
+					  {desc:'k', value:11.0, url:'https://.png', impact_instances:0},
+					  {desc:'l', value:12.0, url:'https://.png', impact_instances:0},
+					  {desc:'m', value:13.0, url:'https://.png', impact_instances:0},
+					  {desc:'n', value:14.0, url:'https://.png', impact_instances:0},
+					  {desc:'o', value:15.0, url:'https://.png', impact_instances:0},
+					  {desc:'p', value:16.0, url:'https://.png', impact_instances:0},
+					  {desc:'q', value:17.0, url:'https://.png', impact_instances:0},
+					  {desc:'r', value:18.0, url:'https://.png', impact_instances:0},
+					  {desc:'s', value:19.0, url:'https://.png', impact_instances:0},
+					  {desc:'t', value:20.0, url:'https://.png', impact_instances:0},
+					  {desc:'u', value:21.0, url:'https://.png', impact_instances:0},
+					  {desc:'v', value:22.0, url:'https://.png', impact_instances:0},
+					  {desc:'w', value:23.0, url:'https://.png', impact_instances:0}];
 	var impactsNum = 0;
+
+	console.log(impactData);
 	
 	if(money <10){}
 	else if(money<=49)  impactsNum = 4;
 	else if(money<=99)  impactsNum = 6;
 	else if(money<=499) impactsNum = 8;
 	else impactsNum = 10;
+
+	console.log('Money: '+money+' impactsNum: '+impactsNum);
 
 	// Generating the random impacts
 	var pickedImpacts = new Array();
@@ -42,17 +44,23 @@ function getImpacts(money)
 	{
 		var idPicked;
 		do{
-			idPicked = Math.floor((Math.random() * impactsNum));
+			idPicked = Math.floor((Math.random() * impactData.length));
 		}while(typeof pickedImpactsId[idPicked] != 'undefined')
 
+		console.log('picked: ' + idPicked);
+
 		pickedImpactsId[idPicked] = 1;
-		pickedImpacts[i] = impactData[idPicked];
+		pickedImpacts[i] = JSON.parse(JSON.stringify(impactData[idPicked]));
 	}
 
+	console.log(pickedImpacts);
+	
 	// sorting desc
 	pickedImpacts.sort(function(a, b) { 
     	return b.value - a.value;
 	});
+
+
 
 	var perImpact = new Array();
 	for(var i = 0; i < impactsNum; ++i)
@@ -62,17 +70,23 @@ function getImpacts(money)
 
 	for(var i = 0; i < impactsNum - 1; ++i)
 	{
-		var instances = Math.floor(perImpact[i] / pickedImpacts[i].value);
-		pickedImpacts[i].instances = instances;
-		var leftover = perImpact[i] - pickedImpacts[i].value * instances;
-		perImpact[i] = pickedImpacts[i].value * instances;
+		var impact_instances = Math.floor(perImpact[i] / pickedImpacts[i].value);
+		console.log('i:'+i+' impacts:'+impact_instances);
+		pickedImpacts[i].impact_instances = impact_instances;
+		var leftover = perImpact[i] - pickedImpacts[i].value * impact_instances;
+		perImpact[i] = pickedImpacts[i].value * impact_instances;
 		perImpact[i+1] = perImpact[i+1] + leftover;
 	}
 
 	var result = new Array();
 	for(var i = 0; i < impactsNum; ++i)
 	{
-		result[i] = [{desc:pickedImpacts[i].desc, num:pickedImpacts[i].instances, url:pickedImpacts[i].url}];
+		var count = 0;
+		if(pickedImpacts[i].impact_instances>0)
+		{
+			result[count] = [{desc:pickedImpacts[i].desc, num:pickedImpacts[i].impact_instances, url:pickedImpacts[i].url}];
+			++count;
+		}
 	}
 	return result;
 }
