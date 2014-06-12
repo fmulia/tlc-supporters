224813474dd397bd22f068f10c0c3e1e2aec920d
lfranslay <lfranslay@paypal.com>
2014-06-12 16:51:42 +0800

    feat (query string) get all get query params into urlParams

--- views/js/impacts.js
+++ views/js/impacts.js
@@ -76,3 +76,18 @@ function getImpacts(money)
 	}
 	return result;
 }
+
+
+//Get all the URL get params
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
